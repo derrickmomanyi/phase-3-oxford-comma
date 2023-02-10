c6dbe4536ef26919a5eeac8b063a37e3315c14ce
Derrick Momanyi <derrick.momanyi@student.moringaschool.com>
2023-02-10 20:57:15 +0300

    Done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  words = array.length
+  case 
+  when words == 1 then array[0]
+  when words == 2 then array.join(" and ")
+  when words >= 3 
+    then 
+  end_word = array.pop
+  whole_word  = array.push("and ").join(", ") + end_word  
+   
+  end
+    
+  
 end
